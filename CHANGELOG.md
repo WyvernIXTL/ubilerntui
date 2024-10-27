d39f9477fdd75d499f27ac7bc18b298bd1de0e1b
Adam McKellar <dev@mckellar.eu>
2024-10-27 15:15:40 +0100

    fix: missing license for dictionary

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,17 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.1.8] - 2024-10-11
+
+### Fixed
+
+* Missing MIT license for dictionary.
+
+### Changed
+
+* Upgraded to `license-fetcher == 0.5.0`.
+
+
 ## [0.1.7] - 2024-10-11
 
 ### Changed
@@ -130,8 +141,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 [cargo about]: https://github.com/EmbarkStudios/cargo-about
 
-[Unreleased]: https://github.com/WyvernIXTL/ubilerntui/compare/v0.1.9...HEAD
-[0.1.9]: https://github.com/WyvernIXTL/ubilerntui/compare/v0.1.6...v0.1.9
+[Unreleased]: https://github.com/WyvernIXTL/ubilerntui/compare/v0.1.8...HEAD
+[0.1.8]: https://github.com/WyvernIXTL/ubilerntui/compare/v0.1.7...v0.1.8
+[0.1.7]: https://github.com/WyvernIXTL/ubilerntui/compare/v0.1.6...v0.1.7
 [0.1.6]: https://github.com/WyvernIXTL/ubilerntui/compare/v0.1.5...v0.1.6
 [0.1.5]: https://github.com/WyvernIXTL/ubilerntui/compare/v0.1.4...v0.1.5
 [0.1.4]: https://github.com/WyvernIXTL/ubilerntui/compare/v0.1.3...v0.1.4
--- build.rs
+++ build.rs
@@ -1,7 +1,32 @@
-use license_fetcher::build_script::generate_package_list_with_licenses;
+use std::concat;
+use std::fs::read_to_string;
+
+use license_fetcher::{build_script::generate_package_list_with_licenses, Package};
 
 fn main() {
-    generate_package_list_with_licenses();
+    let mut packages = generate_package_list_with_licenses();
+
+    packages.push(Package {
+        name: "hyph-de-1996".to_owned(),
+        version: "CTAN-2021.03.21".to_owned(),
+        authors: vec!["Deutschsprachige Trennmustermannschaft <trennmuster@dante.de>".to_owned()],
+        description: Some(
+            "TeX-Trennmuster für die reformierte (2006) deutsche Rechtschreibung".to_owned(),
+        ),
+        homepage: None,
+        repository: Some("https://github.com/hyphenation/tex-hyphen".to_owned()),
+        license_identifier: Some("MIT".to_owned()),
+        license_text: Some(
+            read_to_string(concat!(
+                env!("CARGO_MANIFEST_DIR"),
+                "/dictionary/LICENSE-DICTIONARY.txt"
+            ))
+            .expect("Failed reading license of hyph-de-1996"),
+        ),
+    });
+
+    packages.write();
+
     println!("cargo::rerun-if-changed=build.rs");
     println!("cargo::rerun-if-changed=Cargo.lock");
     println!("cargo::rerun-if-changed=Cargo.toml");
