7f14949f6efd46d1393908b5b7407c6339ed5313
Adam McKellar <dev@mckellar.eu>
2026-04-13 15:56:53 +0200

    chore: update deps and license-fetcher dep

--- build.rs
+++ build.rs
@@ -1,12 +1,19 @@
 use std::concat;
 use std::fs::read_to_string;
 
-use license_fetcher::{build_script::generate_package_list_with_licenses, Package};
+use license_fetcher::build::config::{Config, ConfigBuilder};
+use license_fetcher::build::package_list_with_licenses;
+use license_fetcher::{package, PackageList};
 
 fn main() {
-    let mut packages = generate_package_list_with_licenses();
+    let config: Config = ConfigBuilder::from_build_env()
+        .build()
+        .expect("Failed to build configuration.");
 
-    packages.push(Package {
+    let mut packages: PackageList =
+        package_list_with_licenses(config).expect("Failed to fetch metadata or licenses.");
+
+    packages.push(package! {
         name: "hyph-de-1996".to_owned(),
         version: "CTAN-2021.03.21".to_owned(),
         authors: vec!["Deutschsprachige Trennmustermannschaft <trennmuster@dante.de>".to_owned()],
@@ -25,7 +32,9 @@ fn main() {
         ),
     });
 
-    packages.write();
+    packages
+        .write_package_list_to_out_dir()
+        .expect("Failed to write package list.");
 
     println!("cargo::rerun-if-changed=build.rs");
     println!("cargo::rerun-if-changed=Cargo.lock");
